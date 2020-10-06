c38d9377e7c3ae7da2c0a9f7ab762678f51b560c
Hassan Abbas <habbas375@gmail.com>
2020-10-06 08:21:29 -0500

    added function to add new books to the table

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let numberOfBooks = 0;
+const table = document.getElementById("library-table");
 
 function book(title, author, pages, read = false){
     this.title = title;
@@ -8,5 +10,19 @@ function book(title, author, pages, read = false){
 }
 
 function addBookToLibrary(newBook) {
+    numberOfBooks += 1;
+    addBookToTable(newBook);
     myLibrary.push(newBook);
+}
+
+function addBookToTable(newBook){
+    let row = table.insertRow(numberOfBooks);
+    let titleCell = row.insertCell(0);
+    let authorCell = row.insertCell(1);
+    let pagesCell = row.insertCell(2);
+    let readCell = row.insertCell(3);
+    titleCell.innerText = newBook.title;
+    authorCell.innerText = newBook.author;
+    pagesCell.innerText = newBook.pages;
+    readCell.innerText = newBook.read;
 }
