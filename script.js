b853014a701bb5bdfe9a75eef97309a65ae08ae8
Hassan Abbas <habbas375@gmail.com>
2020-10-06 09:21:20 -0500

    Added functions and button to remove a book from the table and array

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let myLibrary = [];
-let numberOfBooks = 0;
 const table = document.getElementById("library-table");
 
-function book(title, author, pages, read = false){
+function book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -10,19 +9,38 @@ function book(title, author, pages, read = false){
 }
 
 function addBookToLibrary(newBook) {
-    numberOfBooks += 1;
     addBookToTable(newBook);
     myLibrary.push(newBook);
 }
 
+function removeBookFromLibrary(index){
+    myLibrary.splice(index, 1);
+}
+
 function addBookToTable(newBook){
-    let row = table.insertRow(numberOfBooks);
-    let titleCell = row.insertCell(0);
-    let authorCell = row.insertCell(1);
-    let pagesCell = row.insertCell(2);
-    let readCell = row.insertCell(3);
-    titleCell.innerText = newBook.title;
-    authorCell.innerText = newBook.author;
-    pagesCell.innerText = newBook.pages;
-    readCell.innerText = newBook.read;
+    let row = table.insertRow(-1);
+    row.insertCell(0).innerText = newBook.title;
+    row.insertCell(1).innerText = newBook.author;
+    row.insertCell(2).innerText = newBook.pages;
+    row.insertCell(3).innerText = newBook.read;
+    row.insertCell(4).appendChild(addRemoveButton());
+}
+
+function removeBookFromTable(index){
+    table.deleteRow(index);
+}
+
+function addRemoveButton(){
+    const button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.setAttribute("class", "remove-btn");
+    button.innerText = "remove";
+    button.addEventListener("click", remove);
+    return button;
+}
+
+function remove(e){
+    let index = e.target.parentElement.parentElement.rowIndex;
+    removeBookFromTable(index);
+    removeBookFromLibrary(index - 1);
 }
