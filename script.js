2db86323ccbf935964d9b57a497aebd1649103f6
Hassan Abbas <habbas375@gmail.com>
2020-10-07 08:56:45 -0500

    Fully working library but no storage

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ let myLibrary = [];
 const table = document.getElementById("library-table");
 const newBookButton = document.getElementById("new-book-btn");
 const formContainer = document.getElementById("form-container");
+const addBookButton = document.getElementById("add-book");
+const cancelButton = document.getElementById("cancel-btn");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
+
 newBookButton.addEventListener("click", openForm);
+addBookButton.addEventListener("click", addBook);
+cancelButton.addEventListener("click", cancel);
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -29,7 +38,7 @@ function addBookToTable(newBook){
     row.insertCell(0).innerText = newBook.title;
     row.insertCell(1).innerText = newBook.author;
     row.insertCell(2).innerText = newBook.pages;
-    row.insertCell(3).innerText = newBook.read;
+    row.insertCell(3).appendChild(addReadCheckbox(newBook.read));
     row.insertCell(4).appendChild(addRemoveButton());
 }
 
@@ -52,6 +61,42 @@ function remove(e){
     removeBookFromLibrary(index - 1);
 }
 
+function addReadCheckbox(hasRead){
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.checked = hasRead;
+    checkbox.addEventListener("click", changeReadStatus);
+    return checkbox;
+}
+
+function changeReadStatus(e){
+    let index = e.target.parentElement.parentElement.rowIndex - 1;
+    myLibrary[index].toggleRead();
+}
+
 function openForm(){
     formContainer.style.display = "block";
 }
+
+function addBook(){
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = readInput.checked;
+
+    if(title !== '' && author !== '' && pages != ''){
+        addBookToLibrary(new Book(title, author, pages, read));
+        cancel();
+    } else {
+        alert("Please complete the form");
+    }
+}
+
+function cancel(){
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readInput.checked = false;
+    formContainer.style.display = "none";
+}
+
